7b1203d01b5db7601aaf9acb6300234bd2a4efd4
jrrpl <jr@epii.co.uk>
2013-09-20 23:21:10 +0100

    A script to delete and recreate the address base database tables in a database called frederick north

---
+++ src/main/java/uk/co/epii/conservatives/henryaddington/DatabaseUploader.java
@@ -0,0 +1,10 @@
+package uk.co.epii.conservatives.henryaddington;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jrrpl
+ * Date: 20/09/2013
+ * Time: 23:17
+ */
+public class DatabaseUploader {
+}
