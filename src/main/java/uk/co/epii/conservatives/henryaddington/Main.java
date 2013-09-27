499f190de8b31e097b1f5e46a0152597e3f912b3
jrrpl <jr@epii.co.uk>
2013-09-27 18:41:11 +0100

    Added Database Uploader

--- src/main/java/uk/co/epii/conservatives/henryaddington/Main.java
+++ src/main/java/uk/co/epii/conservatives/henryaddington/Main.java
@@ -1,5 +1,11 @@
 package uk.co.epii.conservatives.henryaddington;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sun.util.LocaleServiceProviderPool;
+
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Arrays;
@@ -10,6 +16,8 @@ import java.util.Arrays;
  * Time: 02:47
  */
 public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     private static String imageFormat = "tif";
     private static int columns;
@@ -22,9 +30,20 @@ public class Main {
             rows = Integer.parseInt(args[2]);
             imageSplit(Arrays.copyOfRange(args, 3, args.length));
         }
-        if (args.length > 3 && args[0].equals("DESCRIBED_IMAGE_SPLIT")) {
+        else if (args.length > 3 && args[0].equals("DESCRIBED_IMAGE_SPLIT")) {
             splitDescription = ImageSpliter.getSplitDescription(new File(args[1]), args[2], Integer.parseInt(args[3]));
             imageSplit(Arrays.copyOfRange(args, 4, args.length));
+        }
+        else if (args.length > 0 && args[0].equals("UPLOAD_ADDRESS_BASE_DATA")) {
+            ApplicationContext context = new ClassPathXmlApplicationContext("databaseConnectionBeans.xml");
+            DatabaseUploader databaseUploader = (DatabaseUploader)context.getBean("databaseUploader");
+            if (databaseUploader.cleanDatabase()) {
+                LOG.info("Successfully Cleaned Database");
+                databaseUploader.upload(Arrays.copyOfRange(args, 1, args.length));
+            }
+            else {
+                LOG.info("Failed to Cleaned Database");
+            }
         }
     }
 
@@ -49,4 +68,6 @@ public class Main {
             }
         }
     }
+
+
 }
